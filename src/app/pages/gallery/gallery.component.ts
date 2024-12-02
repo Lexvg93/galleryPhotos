ce2bae76d5cf33df6fc0464ea6c3c1561fd6905a
Alejandro <alejandro_gonzalo92@icloud.com>
2024-12-02 12:49:25 +0100

    se añade conexion en gallery y photo
    se crea conexion mediante input y output

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoComponent } from './photo/photo.component';
+import { Photo } from '../../models/photo';
 
 @Component({
   selector: 'app-gallery',
@@ -8,6 +9,9 @@ import { PhotoComponent } from './photo/photo.component';
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
-export class GalleryComponent {
-
+export class GalleryComponent implements OnInit{
+  photoTest = new Photo();
+  ngOnInit(): void {
+      this.photoTest.url = "https://cdn.pixabay.com/photo/2015/06/22/08/40/child-817373_1280.jpg";
+  }
 }
